d94654f569cb94611524d58f36e935b6ab1bd5ae
ilit A <ilita@ilits-mbp.speedport_w_724v_typ_a_05011603_06_003>
2021-01-07 12:03:05 +0100

    test-action

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,8 +3,8 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
     return (
-        <>
+        <div>
             <MarketingApp />
-        </>
+        </div>
     );
 };
